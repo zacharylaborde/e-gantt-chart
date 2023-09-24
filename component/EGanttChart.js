0b7a727926097d98305a7d5897d576f524016ea2
zacharylaborde <zacharylaborde@gmail.com>
2023-09-24 12:03:19 -0400

    EGanttChart splits into 4 subcomponents.

--- component/EGanttChart.js
+++ component/EGanttChart.js
@@ -1,8 +1,16 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <e-gantt-chart-controller></e-gantt-chart-controller>
+    <e-gantt-chart-table></e-gantt-chart-table>
+    <e-gantt-chart-editor-panel></e-gantt-chart-editor-panel>
+    <e-gantt-chart-notification-panel></e-gantt-chart-notification-panel>
+`;
+
 export class EGanttChart extends HTMLElement {
     constructor() {
         super();
         this.root = this.attachShadow({mode: 'closed'});
-        this.root.innerHTML = 'Hello world';
+        this.root.appendChild(template.content.cloneNode(true));
     }
 }
 
