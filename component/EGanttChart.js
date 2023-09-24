7819d1ef3a43c42ef8261829b91e9f483ca33a3f
zacharylaborde <zacharylaborde@gmail.com>
2023-09-24 11:20:08 -0400

    Added EGanttChart component boilerplate.

--- component/EGanttChart.js
+++ component/EGanttChart.js
@@ -1,3 +1,9 @@
 export class EGanttChart extends HTMLElement {
-
+    constructor() {
+        super();
+        this.root = this.attachShadow({mode: 'closed'});
+        this.root.innerHTML = 'Hello world';
+    }
 }
+
+window.customElements.define('e-gantt-chart', EGanttChart);
