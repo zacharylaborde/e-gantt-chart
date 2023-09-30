5f8ff0da9fbb707ee345e44c41dae327cd50954e
zacharylaborde <zacharylaborde@gmail.com>
2023-09-30 09:06:41 -0400

    Fixed Mocks

--- public/instance.js
+++ public/instance.js
@@ -23,24 +23,24 @@ export const egc_loadTitleFromMemoryCommand = new EGC_LoadCommand("title")
     .repo(egc_inMemoryRepository)
     .presenter(egc_titlePresenter)
     .errorPresenter(egc_errorPresenter)
-    .before(value => mockUpdate("title", value));
+    .before(value => mockLoad("title", value));
 export const egc_updateTitleCommand = new EGC_UpdateCommand("title")
     .repo(egc_inMemoryRepository)
     .presenter(egc_titlePresenter)
     .errorPresenter(egc_errorPresenter)
-    .before(value => mockLoad("title", value));
+    .before(value => mockUpdate("title", value));
 
 
 export const egc_loadDateFromMemoryCommand = new EGC_LoadCommand("date")
     .repo(egc_inMemoryRepository)
     .presenter(egc_datePresenter)
     .errorPresenter(egc_errorPresenter)
-    .before(value => mockUpdate("date", value));
+    .before(value => mockLoad("date", value));
 export const egc_updateDateCommand = new EGC_UpdateCommand("date")
     .repo(egc_inMemoryRepository)
     .presenter(egc_datePresenter)
     .errorPresenter(egc_errorPresenter)
-    .before(value => mockLoad("date", value));
+    .before(value => mockUpdate("date", value));
 
 
 export const egc_loadZoomFromMemoryCommand = new EGC_LoadCommand("zoom")
