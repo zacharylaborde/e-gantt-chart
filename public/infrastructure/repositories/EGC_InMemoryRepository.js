10e30e15862628994e761c81ed3083334be083cf
zacharylaborde <zacharylaborde@gmail.com>
2023-09-30 08:11:04 -0400

    Renamed to InMemoryRepository and moved data.

--- public/infrastructure/repositories/EGC_InMemoryRepository.js
+++ public/infrastructure/repositories/EGC_InMemoryRepository.js
@@ -1,11 +1,5 @@
-const inMemoryGanttChartRepository = {
-    title: "No Title",
-    date: "2023-09-26",
-    zoom: "day"
-}
-
-export class EGC_InMemoryGanttChartRepository {
-    constructor(state = inMemoryGanttChartRepository) {
+export class EGC_InMemoryRepository {
+    constructor(state) {
         this.state = state;
     }
 
--- public/instance.js
+++ public/instance.js
@@ -1,10 +1,16 @@
-import { EGC_InMemoryGanttChartRepository } from "./infrastructure/repositories/EGC_InMemoryRepository.js";
+import { EGC_InMemoryRepository } from "./infrastructure/repositories/EGC_InMemoryRepository.js";
 import { EGC_Presenter } from "./infrastructure/presenters/EGC_Presenter.js";
 import { EGC_UpdateCommand } from "./infrastructure/commands/EGC_UpdateCommand.js";
-import { EGC_LoadFromMemoryCommand } from "./infrastructure/commands/EGC_LoadFromMemoryCommand.js";
+import { EGC_LoadCommand } from "./infrastructure/commands/EGC_LoadCommand.js";
+
+const ganttChartData = {
+    title: "No Title",
+    date: "2023-09-26",
+    zoom: "day"
+}
 
 // repositories
-export const egc_inMemoryGanttChartRepository = new EGC_InMemoryGanttChartRepository();
+export const egc_inMemoryRepository = new EGC_InMemoryRepository(ganttChartData);
 
 // presenters.
 export const egc_titlePresenter = new EGC_Presenter();
@@ -13,37 +19,37 @@ export const egc_zoomPresenter = new EGC_Presenter();
 export const egc_errorPresenter = new EGC_Presenter();
 
 // commands.
-export const egc_loadTitleFromMemoryCommand = new EGC_LoadFromMemoryCommand("title")
-    .repo(egc_inMemoryGanttChartRepository)
+export const egc_loadTitleFromMemoryCommand = new EGC_LoadCommand("title")
+    .repo(egc_inMemoryRepository)
     .presenter(egc_titlePresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockLoad("title", value));
 export const egc_updateTitleCommand = new EGC_UpdateCommand("title")
-    .repo(egc_inMemoryGanttChartRepository)
+    .repo(egc_inMemoryRepository)
     .presenter(egc_titlePresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockLoad("title", value));
 
 
-export const egc_loadDateFromMemoryCommand = new EGC_LoadFromMemoryCommand("date")
-    .repo(egc_inMemoryGanttChartRepository)
+export const egc_loadDateFromMemoryCommand = new EGC_LoadCommand("date")
+    .repo(egc_inMemoryRepository)
     .presenter(egc_datePresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockLoad("date", value));
 export const egc_updateDateCommand = new EGC_UpdateCommand("date")
-    .repo(egc_inMemoryGanttChartRepository)
+    .repo(egc_inMemoryRepository)
     .presenter(egc_datePresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockLoad("date", value));
 
 
-export const egc_loadZoomFromMemoryCommand = new EGC_LoadFromMemoryCommand("zoom")
-    .repo(egc_inMemoryGanttChartRepository)
+export const egc_loadZoomFromMemoryCommand = new EGC_LoadCommand("zoom")
+    .repo(egc_inMemoryRepository)
     .presenter(egc_zoomPresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockLoad("zoom", value));
 export const egc_updateZoomCommand = new EGC_UpdateCommand("zoom")
-    .repo(egc_inMemoryGanttChartRepository)
+    .repo(egc_inMemoryRepository)
     .presenter(egc_zoomPresenter)
     .errorPresenter(egc_errorPresenter)
     .before(value => mockUpdate("zoom", value));
@@ -60,7 +66,7 @@ function mockUpdate(key, value) {
 
 function mockLoad(key, value) {
     return new Promise(resolve => {
-        console.log(`Loading ${key}: ${value}`);
+        console.log(`Loaded ${key}: ${value}`);
         setTimeout(() => {
             resolve();
         }, 200);
