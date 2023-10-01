5a2c01d96841c55d38bf011f9ec55e96727557d9
zacharylaborde <zacharylaborde@gmail.com>
2023-10-01 13:32:59 -0400

    Added some styling to gantt chart

--- public/components/EGC_Table.js
+++ public/components/EGC_Table.js
@@ -3,6 +3,7 @@ import { EGC_Timeline } from "./EGC_Timeline.js";
 export class EGC_Table extends HTMLElement {
     constructor() {
         super();
+        this.setAttribute('part', 'table');
         this.appendChild(new EGC_Timeline());
     }
 }
--- public/components/EGC_Timeline.js
+++ public/components/EGC_Timeline.js
@@ -3,7 +3,8 @@ import {
     egc_inMemoryGanttChart, 
     egc_zoomObserver, 
     egc_numColumsToLoadObserver,
-    egc_timeRangeGeneratorServices
+    egc_timeRangeGeneratorServices,
+    egc_inMemoryGanttChartSettings
 } from "../instance.js";
 
 const template = document.createElement('template');
@@ -15,9 +16,18 @@ template.innerHTML = `
 export class EGC_Timeline extends HTMLElement {
     constructor() {
         super();
+        this.setAttribute('part', 'timeline');
+        this.style.display = 'grid';
+        this.style.gridTemplateColumns = '1fr';
         this.appendChild(template.content.cloneNode(true));
         this.upperTimeline = this.querySelector("#upper-timeline");
         this.lowerTimeline = this.querySelector("#lower-timeline");
+        this.upperTimeline.setAttribute('part', 'upper-timeline');
+        this.lowerTimeline.setAttribute('part', 'lower-timeline');
+        this.upperTimeline.style.gridTemplateColumns = `repeat(${egc_inMemoryGanttChartSettings.getState("numColumnsToLoad")}, 1fr)`;
+        this.lowerTimeline.style.gridTemplateColumns = `repeat(${egc_inMemoryGanttChartSettings.getState("numColumnsToLoad")}, 1fr)`;
+        this.upperTimeline.style.display = 'grid';
+        this.lowerTimeline.style.display = 'grid';
         egc_dateObserver.subscribe(this);
         egc_zoomObserver.subscribe(this);
         egc_numColumsToLoadObserver.subscribe(this);
@@ -30,12 +40,15 @@ export class EGC_Timeline extends HTMLElement {
         this.lowerTimeline.innerHTML = '';
         egc_timeRangeGeneratorServices[zoom].generateUpperTimeline(date).forEach(e => {
             const elem = document.createElement("span");
+            elem.setAttribute('part', 'upper-timeline-dt');
             elem.innerText = e.text;
-            // elem.columnSpan = e.columnCount;     // *** FIX THIS ***    How to change the style so that the column span is wider? Learn grids in css.
+            console.log(e.columnCount);
+            elem.style.gridColumn = `span ${e.columnCount}`
             this.upperTimeline.appendChild(elem);
         });
         egc_timeRangeGeneratorServices[zoom].generateLowerTimeline(date).forEach(e => {
             const elem = document.createElement("span");
+            elem.setAttribute('part', 'lower-timeline-dt');
             elem.innerText = e;
             this.lowerTimeline.appendChild(elem);
         });
