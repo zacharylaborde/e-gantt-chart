b56e6db9412d0703863a73e584612adc20e5bd8a
zacharylaborde <zacharylaborde@gmail.com>
2023-10-02 16:25:48 -0400

    Added columns controller

---
+++ public/components/EGC_ColumnsController.js
@@ -0,0 +1,22 @@
+import { egc_inMemoryGanttChartSettings, egc_numColumsToLoadObserver, egc_updateNumColumnsToLoadCommand } from "../instance.js";
+
+const template = document.createElement('template');
+template.innerHTML = `
+    <input type="number" id="columns-controller"></input>
+`;
+
+export class EGC_ColumnsController extends HTMLElement {
+    constructor() {
+        super();
+        this.appendChild(template.content.cloneNode(true));
+        this.columnsController = this.querySelector('#columns-controller');
+        this.columnsController.onchange = () => egc_updateNumColumnsToLoadCommand.execute(this.columnsController.value);
+        egc_numColumsToLoadObserver.subscribe(this);
+    }
+
+    dataDidUpdate() {
+        this.columnsController.value = egc_inMemoryGanttChartSettings.getState('numColumnsToLoad');
+    }
+}
+
+window.customElements.define('egc-columns-controller', EGC_ColumnsController);
